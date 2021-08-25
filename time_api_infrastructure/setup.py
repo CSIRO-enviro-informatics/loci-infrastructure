ea6b160be0fa1962f4ff3cb564f6ee29cbfc99ce
Peter Baker <peter.baker@anu.edu.au>
2021-08-25 11:42:56 +1000

    Updating setup

--- time_api_infrastructure/setup.py
+++ time_api_infrastructure/setup.py
@@ -1,7 +1,7 @@
 import setuptools
 
 
-with open("README.md") as fp:
+with open("../README.md") as fp:
     long_description = fp.read()
 
 
@@ -9,7 +9,7 @@ setuptools.setup(
     name="time_api_infrastructure",
     version="0.0.1",
 
-    description="An empty CDK Python app",
+    description="Deployment of time demo infrastructure for CSIRO LOCI project.",
     long_description=long_description,
     long_description_content_type="text/markdown",
 
@@ -30,18 +30,10 @@ setuptools.setup(
 
     classifiers=[
         "Development Status :: 4 - Beta",
-
         "Intended Audience :: Developers",
-
-        "Programming Language :: JavaScript",
-        "Programming Language :: Python :: 3 :: Only",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
-
         "Topic :: Software Development :: Code Generators",
         "Topic :: Utilities",
-
         "Typing :: Typed",
     ],
 )
